58a157ba224a52094538d9011df3c2c1b46dbddb
amixo amixo <johan.062@hotmail.com>
2016-09-07 12:21:01 -0500

    V1

---
+++ SQL-NS5-V1.sql
@@ -0,0 +1,5 @@
+--select * from ns5  where descripcionUno LIKE 'ASSEMBLING%'
+--select * from ns5  where descripcionUno LIKE 'ANTENA%'
+select * from ns5  where descripcionUno LIKE 'AIR FILTER%'
+UPDATE NS5 SET Quantity = 2 WHERE ID = 4413
+
---
+++ SQL-SES-V1.sql
@@ -0,0 +1,3 @@
+--select * from ses where Descripcion  LIKE 'ASSEMBLING%' 
+--select * from ses where Descripcion LIKE 'ANTENA%' 
+select * from ses where Descripcion LIKE 'AIR FILTER%' 

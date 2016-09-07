5cadc401911a9a295ee1f6219a4c98fc5e3b0a81
amixo amixo <johan.062@hotmail.com>
2016-09-07 16:14:39 -0500

    V2

--- SQL-NS5-V1.sql
+++ SQL-NS5-V1.sql
@@ -1,5 +1,40 @@
 --select * from ns5  where descripcionUno LIKE 'ASSEMBLING%'
 --select * from ns5  where descripcionUno LIKE 'ANTENA%'
-select * from ns5  where descripcionUno LIKE 'AIR FILTER%'
-UPDATE NS5 SET Quantity = 2 WHERE ID = 4413
+--select * from ns5  where descripcionUno LIKE 'AIR FILTER%'
+--UPDATE NS5 SET Quantity = 2 WHERE ID = 4413
+--SELECT * FROM NS5 WHERE DescripcionUno LIKE 'AIR FILTER%'
+--SELECT * FROM NS5 WHERE DescripcionUno LIKE 'ARC%'
+--SELECT * FROM NS5 WHERE DescripcionUno LIKE 'AUX%'
+--SELECT * FROM NS5 WHERE DescripcionUno LIKE 'ANN%'
+--SELECT * FROM NS5 WHERE DescripcionUno LIKE 'ACRYL%'
+--SELECT * FROM NS5 WHERE DescripcionUno LIKE 'AFT%'
+--SELECT * FROM NS5 WHERE DescripcionUno LIKE 'ADJUST BOLT%'
+--SELECT * FROM NS5 WHERE DescripcionUno LIKE 'ALIGA%'
+--SELECT * FROM NS5 WHERE DescripcionUno LIKE 'ANGULAR%'
+--SELECT * FROM NS5 WHERE DescripcionUno LIKE 'AIR CYLINDER%'
+--UPDATE NS5 SET Quantity = 2 WHERE ID = 3187
+--SELECT * FROM NS5 WHERE DescripcionUno LIKE 'ADJUSTIN%'
+--SELECT * FROM NS5 WHERE DescripcionUno LIKE 'ADAPTOR%'
+--SELECT * FROM NS5 WHERE DescripcionUno LIKE 'AIR BREATHER%'
+--SELECT * FROM NS5 WHERE DescripcionUno LIKE 'AIR HOSE%'
+--SELECT * FROM NS5 WHERE DescripcionUno LIKE 'AIR STAR%'
+--SELECT * FROM NS5 WHERE DescripcionUno LIKE 'AIR SUCTION%'
+SELECT * FROM NS5 WHERE DescripcionUno LIKE 'AIR PISTON%'
+UPDATE NS5 SET Quantity = 2 WHERE ID = 3187
+
+SELECT  COUNT(*) FROM NS5 GROUP BY DescripcionUno HAVING COUNT(*) > 1
+
+SELECT  DescripcionUno FROM NS5 WHERE NS5.DescripcionUno
+IN (SELECT DescripcionUno FROM NS5 AS TMP GROUP BY DescripcionUno HAVING COUNT(*) >1) 
+ORDER BY NS5.DescripcionUno
+
+
+
+
+
+
+
+
+
+
 
--- SQL-SES-V1.sql
+++ SQL-SES-V1.sql
@@ -1,3 +1,34 @@
 --select * from ses where Descripcion  LIKE 'ASSEMBLING%' 
 --select * from ses where Descripcion LIKE 'ANTENA%' 
-select * from ses where Descripcion LIKE 'AIR FILTER%' 
+--select * from ses where Descripcion LIKE 'AIR FILTER%' 
+--select * from ses where Descripcion LIKE 'ARC%' 
+--select * from ses where Descripcion LIKE 'AUX%' 
+--SELECT * FROM SES WHERE Descripcion LIKE 'ANN%'
+--SELECT * FROM SES WHERE Descripcion LIKE 'ACRYL%'
+--SELECT * FROM SES WHERE Descripcion LIKE 'AFT%'
+--SELECT * FROM SES WHERE Descripcion LIKE 'ADJUST%'
+--SELECT * FROM SES WHERE Descripcion LIKE 'ALIGA%'
+--SELECT * FROM SES WHERE Descripcion LIKE 'ANGULAR%'
+--SELECT * FROM SES WHERE Descripcion LIKE 'AIR CYLINDER%'
+--SELECT * FROM SES WHERE Descripcion LIKE 'ADJUSTING%'
+--SELECT * FROM SES WHERE Descripcion LIKE 'ADAPTOR%'
+--SELECT * FROM SES WHERE Descripcion LIKE 'AIR BREATHER%'
+--SELECT * FROM SES WHERE Descripcion LIKE 'AIR HOSE%'
+--SELECT * FROM SES WHERE Descripcion LIKE 'AIR START%'
+--SELECT * FROM SES WHERE Descripcion LIKE 'ADAPTER PLATE%'
+--SELECT * FROM SES WHERE Descripcion LIKE 'AIR ESCAPE%'
+--SELECT * FROM SES WHERE Descripcion LIKE 'AIR SUCTION%'
+SELECT * FROM SES WHERE Descripcion LIKE 'AIR PISTON%'
+
+
+
+
+
+
+
+
+
+
+
+
+
